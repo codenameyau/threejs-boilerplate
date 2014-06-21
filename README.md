563489d7453f111a4186e43c1e57ba8197af7469
Jorge Yau <jyau01@citymail.cuny.edu>
2014-06-20 23:24:47 -0400

    Cleaning up gulpfile and package.json

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ threejs-boilerplate
 Three.js minimal starter project with useful Three.js plugins.
 
 Since many Three.js plugins and shaders do not have individual bower packages,
-bower is not included locally. Instead, Three.js and plugins for it can
+bower is not included locally. Instead, Three.js, its plugins, and dat-gui can
 be found in `assets/js/lib/`.
 
 ###Getting Started
@@ -28,8 +28,11 @@ Visit your browser: `localhost:9000`
 ###Deployment
 Before deploying your project, run: `gulp`
 
-This will generate a `dist/` folder that contains a concatenated and
-minified version of your project files. Use that folder for deployment.
+This will generate a `dist/` folder that contains a concatenated and minified
+version of your JavaScript files. Use the `dist/` folder for deployment.
+
+Update `gulpfile.js` to add additional tasks.
+
 
 ###Future Tasks
-* Create a Yeoman generator to replace the alternative method mention above.
+* Add `gulp-imgagemin` to `gulpfile.js`
--- gulpfile.js
+++ gulpfile.js
@@ -9,54 +9,34 @@
 // Import node packages
 var gulp = require('gulp');
 var rimraf = require('rimraf');
-var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
 
-// Configure paths
 var PATHS = {
-
-  // Order: lib, plugins, main
-  scripts : [
-    'assets/js/lib/*.js',
-    'assets/js/lib/**/*.js',
-    'assets/js/*.js'
-  ],
-
-  // CSS files in assets
-  stylesheets : 'assets/css/*.css'
-
+  images : 'assets/img/**',
 };
-
 
 // Remove last generated dist folder
 gulp.task('clean', function(cb) {
   rimraf('dist/', cb);
 });
 
-
-// Concat and minify scripts
-gulp.task('scripts', ['clean'], function() {
-  return gulp.src(PATHS.scripts)
-    .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'));
-});
-
-// Copy main.css into dist
-gulp.task('css', ['clean'], function() {
-  gulp.src(PATHS.stylesheets)
-    .pipe(gulp.dest('dist/css/'));
-})
-
 // Replace build tags in index.html
-gulp.task('usemin', ['clean'], function(){
+gulp.task('usemin', ['clean'], function() {
   gulp.src('index.html')
     .pipe(usemin({
-      js: [uglify()]
+      css: [rev()],
+      js: [uglify(), rev()],
     }))
     .pipe(gulp.dest('dist/'));
 });
 
+// Copy static images (todo: gulp-imagemin)
+gulp.task('images', ['clean'], function() {
+  gulp.src(PATHS.images)
+    .pipe(gulp.dest('dist/img'));
+});
+
 // Generate dist folder for production
-gulp.task('default', ['scripts', 'css', 'usemin']);
+gulp.task('default', ['clean', 'usemin']);
